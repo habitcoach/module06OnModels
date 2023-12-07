861653515a19935785addaa9aab0cb936039e704
habitcoach <the_habit_coach@outlook.com>
2023-12-07 14:13:58 +0530

    Add project files.

---
+++ module06OnModels/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace module06OnModels.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
